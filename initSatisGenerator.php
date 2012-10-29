0043fd0755b7d25320eaac405c3ed53eeaea36c2
Daniel Fahlke <daniel@digitalmanufaktur.com>
2012-10-29 13:40:45 +0100

    ensure, right branch is checked out for source

--- initSatisGenerator.php
+++ initSatisGenerator.php
@@ -14,6 +14,9 @@ passthru("wget http://getcomposer.org/composer.phar -O $composerBin");
 passthru("php $composerBin create-project --keep-vcs composer/satis $satisDir");
 
 passthru("git clone git@github.com:magento-hackathon/composer-repository.git $magentoComposerRepositorySourceDir");
+chdir($magentoComposerRepositorySourceDir);
+passthru("git checkout master");
+
 passthru("cp -rf $magentoComposerRepositorySourceDir $magentoComposerRepositoryBuildDir");
 
 chdir($magentoComposerRepositoryBuildDir);
