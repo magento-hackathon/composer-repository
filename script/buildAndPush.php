57de33940e412f85fd16784cea40d90ec83620f7
Vinai Kopp <vinai@netzarbeiter.com>
2012-10-29 19:40:50 +0100

    Add lockfile to avoid parallel processes

--- script/buildAndPush.php
+++ script/buildAndPush.php
@@ -7,7 +7,19 @@ $composerBin = $rootDir . 'composer.phar';
 $satisDir = $rootDir . 'satis';
 $magentoComposerRepositorySourceDir = $rootDir . 'magentoPackagistSource';
 $magentoComposerRepositoryBuildDir  = $rootDir . 'magentoPackagistBuild';
+$lockFile = $magentoComposerRepositorySourceDir . '/build.lock';
+$lockTimeout = 60 * 60; // Max validity of a lockfile in seconds
 
+
+if (file_exists($lockFile)) {
+    if (time() - filectime($lockFile) < $lockTimeout) {
+        $lockFile = implode('/', array_slice(explode('/', realpath($lockFile)), -3));
+        echo "Lockfile ...{$lockFile} exists, quitting.\n";
+        exit();
+    }
+    unlink($lockFile); // Lockfile is stale
+}
+touch($lockFile);
 
 chdir($magentoComposerRepositorySourceDir);
 passthru("git pull ");
@@ -21,8 +33,9 @@ passthru("php {$satisDir}/bin/satis build {$magentoComposerRepositorySourceDir}/
 
 chdir($magentoComposerRepositoryBuildDir);
 passthru("git add .");
-passthru("git commit -a -m \"updated repository by Buildscript\" ");
+passthru("git commit -a -m \"Updated repository by Buildscript\" ");
 
 //passthru("git push --dry-run");
 passthru("git push");
 
+unlink($lockFile);
