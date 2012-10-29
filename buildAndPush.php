c4403bf06b8531bb0c618981a72e52c23fb39a7a
Daniel Fahlke <daniel@digitalmanufaktur.com>
2012-10-29 12:44:19 +0100

    add scripts for automatic building

---
+++ buildAndPush.php
@@ -0,0 +1,26 @@
+#!/usr/bin/env php
+<?php
+
+
+$rootDir  = __DIR__ . '/../';
+$composerBin = $rootDir . 'composer.phar';
+$satisDir = $rootDir . 'satis';
+$magentoComposerRepositorySourceDir = $rootDir . 'magentoPackagistSource';
+$magentoComposerRepositoryBuildDir  = $rootDir . 'magentoPackagistBuild';
+
+
+chdir($magentoComposerRepositorySourceDir);
+passthru("git pull ");
+
+chdir($magentoComposerRepositoryBuildDir);
+passthru("git pull");
+
+chdir($rootDir);
+
+passthru("php {$satisDir}/bin/satis build {$magentoComposerRepositorySourceDir}/satis.json {$magentoComposerRepositoryBuildDir}/");
+
+chdir($magentoComposerRepositoryBuildDir);
+passthru("git add .");
+passthru("git commit -a -m \"updated repository by Buildscript\" ");
+passthru("git push --dry-run");
+
---
+++ initSatisGenerator.php
@@ -0,0 +1,25 @@
+#!/usr/bin/env php
+<?php
+
+
+$rootDir  = __DIR__ . '/../';
+$composerBin = $rootDir . 'composer.phar';
+$satisDir = $rootDir . 'satis';
+$magentoComposerRepositorySourceDir = $rootDir . 'magentoPackagistSource';
+$magentoComposerRepositoryBuildDir  = $rootDir . 'magentoPackagistBuild';
+
+chdir($rootDir);
+
+passthru("wget http://getcomposer.org/composer.phar -O $composerBin");
+passthru("php $composerBin create-project --keep-vcs composer/satis $satisDir");
+
+passthru("git clone git@github.com:magento-hackathon/composer-repository.git $magentoComposerRepositorySourceDir");
+passthru("cp -rf $magentoComposerRepositorySourceDir $magentoComposerRepositoryBuildDir");
+
+chdir($magentoComposerRepositoryBuildDir);
+
+passthru("git checkout  gh-pages");
+
+#passthru("git clone git://github.com/composer/satis.git $satisDir");
+#chdir($satisDir);
+#passthru("composer");
